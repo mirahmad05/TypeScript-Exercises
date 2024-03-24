0dfa3f7c84a36de15f532399396aa515dd91015a
mirahmad05 <160038951+mirahmad05@users.noreply.github.com>
2024-03-24 13:16:24 +0500

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-let personName = '';
-personName = prompt('What is your Name') || '';
-if (personName !== null && personName !== '') {
-    alert(`Hello ${personName}, would you like to learn some Python today`);
-    (`You have to fill you Name`);
-}
-console.log(personName);
